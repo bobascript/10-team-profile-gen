33a66f4a8c1b44c0aff43c8c277065fd6671085c
mintghostii <alvarezjavi960@gmail.com>
2023-03-04 15:39:13 -0500

    added template for html and css, also added employee, engineer, intern, and manager files to lib. Also started on index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// Importing the required modules
+const inquirer=require("inquirer")
+const fs=require("fs")
+const Employee=require("./lib/Employee.js")
+const Employee=require("./lib/Manager.js")
+const Employee=require("./lib/Engineer.js")
+const Employee=require("./lib/Intern.js")
